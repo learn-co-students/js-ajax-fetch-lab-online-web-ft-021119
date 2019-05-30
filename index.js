ed12677dcc12630b1a1ea74cfe1b56269ffaf12b
bebopvinh <vinhdo07@gmail.com>
2019-05-30 15:58:44 -0700

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const token = '7be369911bd4e093b1aa9ce86436e179502b3b9c';
-
-fetch('https://api.github.com/user/repos', {
-  headers: {
-    Authorization: `token ${token}`
-  }
-})
-  .then(res => res.json())
-  .then(json => console.log(json));
+const githubAPI = 'https://api.github.com'
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+  // return token
+  return ''
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  const repo = 'learn-co-curriculum/js-ajax-fetch-lab'
+  const postData = ''
   //use fetch to fork it!
+  fetch(`${githubAPI}/repos/${repo}/forks`,
+    {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        Authorization: ""
+      }
+    }
+  )
 }
 
 function showResults(json) {
